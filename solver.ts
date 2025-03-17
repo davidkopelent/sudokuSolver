76e7dfcc8500fc75ca54f2cfd1f7d39aa69c4051
David Kopelent <66686489+davidkopelent@users.noreply.github.com>
2025-03-17 20:13:12 +0000

    Initial commit

---
+++ solver.ts
@@ -0,0 +1,69 @@
+function solveSudoku(board: string[][]): void {
+    const solver = new SudokuSolver(board);
+    const solvedBoard = solver.solve();
+
+    for (let i = 0; i < 9; i++) {
+        for (let j = 0; j < 9; j++) {
+            board[i][j] = solvedBoard[i][j];
+        }
+    }
+}
+
+class SudokuSolver {
+    private board: string[][];
+
+    constructor(board: string[][]) {
+        this.board = board;
+    }
+
+    public solve(): string[][] {
+        this.backtrack();
+        return this.board;
+    }
+
+    private backtrack(): boolean {
+        for (let row = 0; row < 9; row++) {
+            for (let col = 0; col < 9; col++) {
+                if (this.board[row][col] === ".") {
+                    for (let num = 1; num <= 9; num++) {
+                        let numStr = num.toString();
+                        if (this.isValid(row, col, numStr)) {
+                            this.board[row][col] = numStr;
+                            if (this.backtrack()) {
+                                return true;
+                            }
+                            this.board[row][col] = ".";
+                        }
+                    }
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+
+    private isValid(row: number, col: number, num: string): boolean {
+        for (let i = 0; i < 9; i++) {
+            if (this.board[row][i] === num || this.board[i][col] === num) {
+                return false;
+            }
+        }
+
+        const boxRowStart = Math.floor(row / 3) * 3;
+        const boxColStart = Math.floor(col / 3) * 3;
+
+        for (let i = 0; i < 3; i++) {
+            for (let j = 0; j < 3; j++) {
+                if (this.board[boxRowStart + i][boxColStart + j] === num) {
+                    return false;
+                }
+            }
+        }
+
+        return true;
+    }
+
+    public getBoard(): string[][] {
+        return this.board;
+    }
+}
