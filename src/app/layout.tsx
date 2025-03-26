6a2f91204c101e51b15c15ff0e8edb5d0d288ac5
David Kopelent <davidekkopelent@gmail.com>
2025-03-26 10:17:01 +0100

    Update: Added analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import './globals.css';
 import type { Metadata } from 'next'
 import Navbar from '@/components/Navbar';
+import { Analytics } from "@vercel/analytics/react"
 
 export const metadata: Metadata = {
     title: 'Sudoku Solver',
@@ -23,6 +24,7 @@ export default function RootLayout({
                     {children}
                 </main>
             </body>
+            <Analytics />
         </html>
     )
 }
